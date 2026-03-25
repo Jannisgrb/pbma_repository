b3f2a456051d0cd5a2ac86be83a9a8f5dda1cf21
swt <swt@pb25arm>
2026-03-25 01:57:11 +0100

    macTest

--- OOP_A3/main.cpp
+++ OOP_A3/main.cpp
@@ -19,7 +19,7 @@ int ggt_2 = ggt2(45, 100);
 cout << "Größter gemeinsamer Teiler von " << x << " & " << y << ": " << ggt << endl;
 cout << "Größter gemeinsamer Teiler von " << x << " & " << y << ": " << ggt_2 << endl;
 
-FRRR?
+Das ist die Mac version!!!
 
 return 0;
 }
