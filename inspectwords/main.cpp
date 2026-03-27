0c1ed6bfc61003b7d9a5a43ef8358b9e599194bc
swt <swt@pb25>
2026-03-27 14:35:51 +0100

    wetergearbeitet an ADS

--- inspectwords/main.cpp
+++ inspectwords/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int word_counter(vector<string> words);
 int letters_counter(vector<string> words);
+int longest_word(vector<string> words);
+int shortest_word(vector<string> words);
 
 int main(int argc, const char* argv[]){
 	args_t args(argc, argv);
@@ -15,13 +17,16 @@ int main(int argc, const char* argv[]){
 
 	cout << "Anzahl der Wörter: " << word_counter(woerter) << endl;
 	cout << "Anzahl der Buchstaben: " << letters_counter(woerter) << endl;
-
+	cout << "Länge des längsten Wortes: " << longest_word(woerter) << endl;
+	cout << "Länge des kürzesten Wortes: " << shortest_word(woerter) << endl;
 
 }
+
 
 int word_counter(vector<string> words){
 	return words.size();
 }
+
 
 int letters_counter(vector<string> words){
 	int counter = 0;
@@ -29,4 +34,26 @@ int letters_counter(vector<string> words){
 		counter = counter + words[i].size();
 	}
 	return counter;
+}
+
+
+int longest_word(vector<string> words){
+	unsigned int longest = words[1].size();
+	for(unsigned int i = 0; i < words.size(); i++){
+		if (words[i].size() > longest){
+			longest = words[i].size();
+		}
+	}
+	return longest;
+}
+
+
+int shortest_word(vector<string> words){
+	unsigned int shortest = words[1].size();
+	for(unsigned int i = 0; i < words.size(); i++){
+		if (words[i].size() < shortest){
+			shortest = words[i].size();
+		}
+	}
+	return shortest;
 }
