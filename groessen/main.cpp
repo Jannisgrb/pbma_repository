46f400b872e001d5c406d5e91d214ea17a7ec78d
n3030503 <n3030503@x2go>
2026-03-30 15:20:51 +0200

    Susi Sinnlos finished

--- groessen/main.cpp
+++ groessen/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "pbma.h"
+#include <vector>
 using namespace std;
 
 struct Person {
@@ -10,29 +11,60 @@ struct Person {
 };
 
 void person_out(Person p){
-	for(int i = 0; i < 17; i++){
-		cout << p.vorname << p.nachname << p.alter << ": " << p.punkte << endl;
-	}
+		cout << p.vorname << " " << p.nachname << " " << p.alter << ": ";
+		for(int i = 0; i < 10; i++){
+			cout << p.punkte[i] << " ";
+		}
+		cout << endl;;
 }
 
-int person_in(Person b){
+Person person_in(int age, vector<int> *zahlen, int startindex){
+	Person p;
+	p.vorname = "Susi";
+	p.nachname = "Sinnlos";
+	p.alter = age;
+	for(int i = 0; i<10; i++){
+		p.punkte[i] = zahlen->at(startindex * 10 + i);
+	}
 
+	return p;
+}
+
+int p_sum(Person p){
+	int sum = 0;
+	for(int i = 0; i < 10; i++){
+		sum = sum + p.punkte[i];
+	}
+	return sum;
+}
+
+int beste_susi(vector<Person> *p){
+	int best_age = p->at(1).alter;
+	int best_score = 0;
+
+	for(int i = 0; i < 17; i++){
+		if(p_sum(p->at(i)) > best_score){
+			best_score = p_sum(p->at(i));
+			best_age = p->at(i).alter;
+		}
+	}
+	return best_age;
 }
 
 
 int main(){
 	vector<int> zahlen = create_same_randints(170, 0, 20);
 	vector<Person> personen;
-	for(unsigned int i = 0; i < 17; i++){
-		personen[i] = Person{"Susi", "Sinnlos", i+18};
-		for(int j = 0; j < 10; j++){
-			personen[i].punkte[j] = zahlen[j];
-		}
+
+	for(int i = 0; i < 17; i++){
+		personen.push_back(person_in(i+18, &zahlen, i));
 	}
 
 	for (size_t i = 0; i < 17; i++){
 		person_out(personen[i]);
 	}
+
+	cout << "Die beste Susi hat das Alter: " << beste_susi(&personen) << endl;
 
 
 }
