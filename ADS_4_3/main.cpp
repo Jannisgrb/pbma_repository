1dd7dbcd8bf9a8a51f79de68f5a2b0b3c3345ece
n3030503 <n3030503@x2go>
2026-04-30 10:37:26 +0200

    heapsort fertig

--- ADS_4_3/main.cpp
+++ ADS_4_3/main.cpp
@@ -1,36 +1,55 @@
+
 #include <iostream>
 #include <vector>
 #include "pbma.h"
 using namespace std;
 
 void HeapSort(vector<int> &list);
-void bubble_up(vector<int> &baum);
-void bubble_down(vector<int> &baum);
+void bubble_up(vector<int> &baum, int i);
+void bubble_down(vector<int> &baum, int n);
 
 int main() {
-	check_sort(HeapSort, true);
+	check_sort(HeapSort, true, true);
 }
 
 void HeapSort(vector<int> &list) {
 	vector<int> baum(list.size() + 1);
 	baum[1] = list[0];
-	for (size_t i = 2; i < list.size(); i++) {
-		baum[i] = list[i - 1];
-		bubble_up(baum);
+	for (size_t i = 0; i < list.size(); i++) {
+		int akt_index = i + 1;
+		baum[akt_index] = list[i];
+		bubble_up(baum, akt_index);
+	}
+
+	for (size_t i = 0; i < list.size(); i++){
+		list[i] = baum[1];
+		int last_index = list.size() - i;
+		baum[1] = baum[last_index];
+		bubble_down(baum, last_index - 1);
 	}
 }
 
-void bubble_up(vector<int> &baum) { //Muss von unten nach oben abfragen (und nicht wie gerade von oben nach unten)
-	for (size_t i = 1; i < baum.size(); i++) {
-		if(baum[i] > baum[i*2]){
-			swap(baum[i], baum[i*2]);
-		}
-		else if(baum[i] > baum[i*2+1]){
-			swap(baum[i], baum[i*2+1]);
-		}
+void bubble_up(vector<int> &baum, int i) {
+	while(i > 1 && baum[i/2] > baum[i]){
+		swap(baum[i], baum [i/2]);
+		i = i / 2;
 	}
 }
 
-void bubble_down(vector<int> &baum) {
+void bubble_down(vector<int> &baum, int n) {
+	int i = 1;
+	while(2*i <= n){
+		int child = 2*i;
 
+		if(child + 1 <= n && baum[child+1] < baum[child]){
+			child++;
+		}
+
+		if(baum[child] < baum[i]){
+			swap(baum[i], baum[child]);
+			i = child;
+		}else {
+			break;
+		}
+	}
 }
