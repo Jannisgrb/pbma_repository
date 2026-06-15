a80de3660bb6538c98db6d441bd9fa9a12aae583
swt <swt@pb25arm>
2026-06-15 14:34:28 +0200

    OOP_7_2

--- OOP_7_2/Sequence.h
+++ OOP_7_2/Sequence.h
@@ -1,60 +1,89 @@
 #ifndef SEQUENCE_H
 #define SEQUENCE_H
 
-#include <cstddef>     // Für size_t
-#include <stdexcept>    // Für std::runtime_error
+#include <iostream>
+#include <cstddef>
+#include <stdexcept>
 #include <string>
-#include <algorithm>   // Für std::swap
+#include <algorithm>
 
-template <typename T>
+template<typename T>
 class Sequence {
 public:
-    // Standardkonstruktor mit initialer Startkapazität
-    Sequence(size_t cap = 10);
-    Sequence(const Sequence& other);
-    Sequence& operator=(const Sequence& other);
-    ~Sequence();
+	Sequence(size_t cap = 10);
+	Sequence(const Sequence &other);
+	Sequence& operator=(const Sequence &other);
+	~Sequence();
 
-    size_t size() const;
-    size_t capacity() const;
-    bool is_empty() const;
-    bool is_full() const;
+	size_t size() const;
+	size_t capacity() const;
+	bool is_empty() const;
+	bool is_full() const;
 
-    void clear();
-    void trim();
+	void clear();
+	void trim();
 
-    T& operator[](size_t pos);
-    const T& operator[](size_t pos) const;
+	T& operator[](size_t pos);
+	const T& operator[](size_t pos) const;
 
-    void insert(size_t pos, const T& ele);
-    void push_back(const T& ele);
-    void remove(size_t pos);
-    bool remove_ele(const T& ele);
+	void insert(size_t pos, const T &ele);
+	void push_back(const T &ele);
+	void remove(size_t pos);
+	bool remove_ele(const T &ele);
 
-    bool operator==(const Sequence& other) const;
+	bool operator==(const Sequence &other) const;
 
 private:
-    void reserve(size_t new_cap);
+	void reserve(size_t new_cap);
 
-    T* _array;
-    size_t _cap;
-    size_t _size;
+	T *_array;
+	size_t _cap;
+	size_t _size;
 };
 
-typedef struct {
-    int matrikelnummer;
-    std::string vorname;
-    std::string nachname;
+class studi {
+private:
+	int matrikelnummer = 0;
+	std::string vorname = "";
+	std::string nachname = "";
+public:
+	int get_matr() {
+		return matrikelnummer;
+	}
 
-    bool operator==(const studi&other) const {
-        return matrikelnummer == other.matrikelnummer;
-    }
+	std::string get_vn() {
+		return vorname;
+	}
 
-    bool operator!=(const studi&other) const {
-        return matrikelnummer != other.matrikelnummer;
-    }
+	std::string get_nn() {
+		return nachname;
+	}
 
-} studi;
+	int& set_matr() {
+		return matrikelnummer;
+	}
+
+	std::string& set_vn() {
+		return vorname;
+	}
+
+	std::string& set_nn() {
+		return nachname;
+	}
+
+	void out(){
+		std::cout << matrikelnummer << " " << vorname << " " << nachname << std::endl;
+	}
+
+	bool operator==(const studi &other) const {
+		return matrikelnummer == other.matrikelnummer;
+	}
+
+	bool operator!=(const studi &other) const {
+		return matrikelnummer != other.matrikelnummer;
+	}
+
+};
 
 #include "sequence.cpp"
 
--- OOP_7_2/seqstud.cpp
+++ OOP_7_2/seqstud.cpp
@@ -3,19 +3,116 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]){
-    args_t args(argc, argv);
-    string fname = args.pos(0, "studis.txt");
+Sequence<studi> search_matr(Sequence<studi> studis, int search_matr) {
+	Sequence<studi> results;
+	for (size_t i = 0; i < studis.size(); ++i) {
+		if (studis[i].get_matr() == search_matr) {
+			results.push_back(studis[i]);
+		}
+	}
+	return results;
+}
 
-    vector<string> lines = read_lines(fname);
+Sequence<studi> search_vn(Sequence<studi> studis, string search_vn) {
+	Sequence<studi> results;
+	for (size_t i = 0; i < studis.size(); ++i) {
+		if (studis[i].get_vn().find(search_vn) != string::npos) {
+			results.push_back(studis[i]);
+		}
+	}
+	return results;
 
-    Sequence<studi> studis;
-    for(size_t i = 0; i < lines.size(); i += 3){
-        studi s;
-        s.matrikelnummer = lines[i];
-        s.vorname = lines[i + 1];
-        s.nachname = lines[i + 2];
+}
 
-        studis.push_back(s);
-    }
+Sequence<studi> search_nn(Sequence<studi> studis, string search_nn) {
+	Sequence<studi> results;
+	for (size_t i = 0; i < studis.size(); ++i) {
+		if (studis[i].get_nn().find(search_nn) != string::npos) {
+			results.push_back(studis[i]);
+		}
+	}
+	return results;
+
+}
+
+Sequence<studi> search_studi(Sequence<studi> studis, string search_matr,
+		string search_vn, string search_nn) {
+	Sequence<studi> results;
+	for (size_t i = 0; i < studis.size(); ++i) {
+		bool nn_ok = search_nn.empty()
+				|| (studis[i].get_nn().find(search_nn) != string::npos);
+		bool vn_ok = search_vn.empty()
+				|| (studis[i].get_vn().find(search_vn) != string::npos);
+		bool matr_ok = search_matr.empty()
+				|| (studis[i].get_matr() == stoi(search_matr));
+
+		if (matr_ok && vn_ok && nn_ok) {
+			results.push_back(studis[i]);
+		}
+	}
+	return results;
+}
+
+int main(int argc, const char *argv[]) {
+	args_t args(argc, argv);
+	string fname = args.pos(0, "studis.txt");
+
+	vector<string> lines = read_words(fname);
+
+	Sequence<studi> studis;
+	for (size_t i = 0; i < lines.size(); i += 3) {
+		studi s;
+		s.set_matr() = stoi(lines[i]);
+		s.set_vn() = lines[i + 1];
+		s.set_nn() = lines[i + 2];
+
+		studis.push_back(s);
+	}
+
+	string matr_wanted = "";
+	string vn_wanted = "";
+	string nn_wanted = "";
+
+	if (args.has_option("m"))
+		matr_wanted = args.option("m");
+	if (args.has_option("v"))
+		vn_wanted = args.option("v");
+	if (args.has_option("n"))
+		nn_wanted = args.option("n");
+
+	Sequence<studi> Result = search_studi(studis, matr_wanted, vn_wanted, nn_wanted);
+
+	for(size_t i = 0; i < Result.size(); ++i){
+		Result[i].out();
+	}
+
+//	if (args.has_option("m")) {
+//		string matr_wanted = args.option("m");
+//		if (!matr_wanted.empty()) {
+//			Sequence<studi> Result = search_matr(studis, stoi(matr_wanted));
+//			for (size_t i = 0; i < Result.size(); ++i) {
+//				Result[i].out();
+//			}
+//		}
+//	}
+//
+//	if (args.has_option("v")) {
+//		string vn_wanted = args.option("v");
+//		if (!vn_wanted.empty()) {
+//			Sequence<studi> Result = search_vn(studis, vn_wanted);
+//			for (size_t i = 0; i < Result.size(); ++i) {
+//				Result[i].out();
+//			}
+//		}
+//	}
+//
+//	if (args.has_option("n")) {
+//			string nn_wanted = args.option("n");
+//			if (!nn_wanted.empty()) {
+//				Sequence<studi> Result = search_nn(studis, nn_wanted);
+//				for (size_t i = 0; i < Result.size(); ++i) {
+//					Result[i].out();
+//				}
+//			}
+//		}
 }
